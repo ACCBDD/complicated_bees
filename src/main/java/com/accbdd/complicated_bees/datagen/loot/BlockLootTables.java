3aaa743c1ef30a8bdaed13bfbc6a44e358005519
ACCBDD <9992251+ACCBDD@users.noreply.github.com>
2024-07-16 12:09:16 -0700

    silk touch and fortune working

--- src/main/java/com/accbdd/complicated_bees/datagen/loot/BlockLootTables.java
+++ src/main/java/com/accbdd/complicated_bees/datagen/loot/BlockLootTables.java
@@ -10,12 +10,21 @@ import net.minecraft.data.loot.BlockLootSubProvider;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.tags.ItemTags;
 import net.minecraft.world.flag.FeatureFlags;
+import net.minecraft.world.item.enchantment.Enchantments;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.storage.loot.LootContext;
 import net.minecraft.world.level.storage.loot.LootPool;
 import net.minecraft.world.level.storage.loot.LootTable;
 import net.minecraft.world.level.storage.loot.entries.LootItem;
+import net.minecraft.world.level.storage.loot.entries.LootPoolEntryContainer;
+import net.minecraft.world.level.storage.loot.functions.ApplyBonusCount;
+import net.minecraft.world.level.storage.loot.functions.CopyNbtFunction;
+import net.minecraft.world.level.storage.loot.functions.LootItemFunctionType;
 import net.minecraft.world.level.storage.loot.functions.SetItemCountFunction;
+import net.minecraft.world.level.storage.loot.predicates.LootItemCondition;
+import net.minecraft.world.level.storage.loot.predicates.LootItemRandomChanceCondition;
 import net.minecraft.world.level.storage.loot.predicates.MatchTool;
+import net.minecraft.world.level.storage.loot.providers.nbt.ContextNbtProvider;
 import net.minecraft.world.level.storage.loot.providers.number.ConstantValue;
 
 import java.util.Collections;
@@ -42,20 +51,29 @@ public class BlockLootTables extends BlockLootSubProvider {
 
     protected static LootTable.Builder nestLootTable(Block beehive) {
         return LootTable.lootTable()
-                .withPool(LootPool.lootPool().when(HAS_SILK_TOUCH).setRolls(ConstantValue.exactly(1.0f)).add(LootItem.lootTableItem(beehive)))
                 .withPool(LootPool.lootPool()
-                        .when(MatchTool.toolMatches(ItemPredicate.Builder.item().of(ItemTagGenerator.SCOOP_TOOL)))
+                        .when(HAS_SILK_TOUCH)
+                        .setRolls(ConstantValue.exactly(1.0f))
+                        .add(
+                                LootItem.lootTableItem(beehive).apply(CopyNbtFunction
+                                        .copyData(ContextNbtProvider.BLOCK_ENTITY)
+                                        .copy("species", "BlockEntityTag.species", CopyNbtFunction.MergeStrategy.REPLACE)
+                                )
+                        ))
+                .withPool(LootPool.lootPool()
+                        .when(HAS_NO_SILK_TOUCH)
                         .setRolls(ConstantValue.exactly(1.0f))
                         .add(
                                 LootItem.lootTableItem(ItemsRegistration.PRINCESS).apply(InheritHiveSpeciesFunction.set())
                         ))
                 .withPool(LootPool.lootPool()
-                        .when(MatchTool.toolMatches(ItemPredicate.Builder.item().of(ItemTagGenerator.SCOOP_TOOL)))
+                        .when(HAS_NO_SILK_TOUCH)
                         .setRolls(ConstantValue.exactly(1.0f))
                         .add(
                                 LootItem.lootTableItem(ItemsRegistration.DRONE)
                                         .apply(InheritHiveSpeciesFunction.set())
-                                        .apply(SetItemCountFunction.setCount(ConstantValue.exactly(2)))
+                                        .apply(SetItemCountFunction.setCount(ConstantValue.exactly(1)))
+                                        .apply(ApplyBonusCount.addBonusBinomialDistributionCount(Enchantments.BLOCK_FORTUNE, 0.5f, 1))
                         )
                 );
     }
