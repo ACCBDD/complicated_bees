7a895fdd4d0f76cf8e80b6946c08c427c94a0ba6
ACCBDD <9992251+ACCBDD@users.noreply.github.com>
2024-08-15 10:49:53 -0700

    make widget dynamic with line heights

--- src/main/java/com/accbdd/complicated_bees/screen/widget/AnalyzerScrollWidget.java
+++ src/main/java/com/accbdd/complicated_bees/screen/widget/AnalyzerScrollWidget.java
@@ -26,10 +26,12 @@ public class AnalyzerScrollWidget extends AbstractScrollWidget {
     private static final int LINE_HEIGHT = 12;
 
     private final AnalyzerMenu menu;
+    private int nextLine;
 
     public AnalyzerScrollWidget(int pX, int pY, int pWidth, int pHeight, AnalyzerMenu menu) {
         super(pX, pY, pWidth, pHeight, Component.empty());
         this.menu = menu;
+        nextLine = 0;
     }
 
     @Override
@@ -70,28 +72,43 @@ public class AnalyzerScrollWidget extends AbstractScrollWidget {
     }
 
     private void drawGeneInfo(GuiGraphics graphics, ItemStack bee) {
-        drawText(graphics, Component.literal("Active"), ACTIVE_COL, 2, 0xFFFFFF);
-        drawText(graphics, Component.literal("Inactive"), INACTIVE_COL, 2, 0xFFFFFF);
+        nextLine = 2;
+        drawText(graphics, Component.literal("Active"), ACTIVE_COL, nextLine, 0xFFFFFF);
+        drawText(graphics, Component.literal("Inactive"), INACTIVE_COL, nextLine, 0xFFFFFF);
+        lineBreak();
+        lineBreak();
 
-        drawGeneValues(graphics, Component.translatable("gui.complicated_bees.species_label"), bee, GeneRegistration.SPECIES.get(), 16);
-        drawGeneValues(graphics, Component.translatable("gui.complicated_bees.lifespan_label"), bee, GeneRegistration.LIFESPAN.get(), 28);
-        drawGeneValues(graphics, Component.translatable("gui.complicated_bees.productivity_label"), bee, GeneRegistration.PRODUCTIVITY.get(), 40);
-        drawGeneValues(graphics, Component.translatable("gui.complicated_bees.flower_label"), bee, GeneRegistration.FLOWER.get(), 52);
+        drawGeneValues(graphics, Component.translatable("gui.complicated_bees.species_label"), bee, GeneRegistration.SPECIES.get());
+        drawGeneValues(graphics, Component.translatable("gui.complicated_bees.lifespan_label"), bee, GeneRegistration.LIFESPAN.get());
+        drawGeneValues(graphics, Component.translatable("gui.complicated_bees.productivity_label"), bee, GeneRegistration.PRODUCTIVITY.get());
+        drawGeneValues(graphics, Component.translatable("gui.complicated_bees.flower_label"), bee, GeneRegistration.FLOWER.get());
+        lineBreak();
 
-        drawTolerantGeneValues(graphics, Component.translatable("gui.complicated_bees.humidity_label"), bee, GeneRegistration.HUMIDITY.get(), 64);
-        drawTolerantGeneValues(graphics, Component.translatable("gui.complicated_bees.temperature_label"), bee, GeneRegistration.TEMPERATURE.get(), 88);
+        drawTolerantGeneValues(graphics, Component.translatable("gui.complicated_bees.humidity_label"), bee, GeneRegistration.HUMIDITY.get());
+        drawTolerantGeneValues(graphics, Component.translatable("gui.complicated_bees.temperature_label"), bee, GeneRegistration.TEMPERATURE.get());
+        lineBreak();
 
-        drawGeneValues(graphics, Component.translatable("gui.complicated_bees.effect_label"), bee, GeneRegistration.EFFECT.get(), 112);
-        drawGeneValues(graphics, Component.translatable("gui.complicated_bees.fertility_label"), bee, GeneRegistration.FERTILITY.get(), 124);
+        drawGeneValues(graphics, Component.translatable("gui.complicated_bees.effect_label"), bee, GeneRegistration.EFFECT.get());
+        drawGeneValues(graphics, Component.translatable("gui.complicated_bees.fertility_label"), bee, GeneRegistration.FERTILITY.get());
+        lineBreak();
 
-        drawGeneValues(graphics, Component.translatable("gui.complicated_bees.diurnal_label"), bee, GeneRegistration.DIURNAL.get(), 142);
-        drawGeneValues(graphics, Component.translatable("gui.complicated_bees.nocturnal_label"), bee, GeneRegistration.NOCTURNAL.get(), 154);
-        drawGeneValues(graphics, Component.translatable("gui.complicated_bees.cave_dwelling_label"), bee, GeneRegistration.CAVE_DWELLING.get(), 166);
-        drawGeneValues(graphics, Component.translatable("gui.complicated_bees.weatherproof_label"), bee, GeneRegistration.WEATHERPROOF.get(), 178);
+        drawGeneValues(graphics, Component.translatable("gui.complicated_bees.diurnal_label"), bee, GeneRegistration.DIURNAL.get());
+        drawGeneValues(graphics, Component.translatable("gui.complicated_bees.nocturnal_label"), bee, GeneRegistration.NOCTURNAL.get());
+        drawGeneValues(graphics, Component.translatable("gui.complicated_bees.cave_dwelling_label"), bee, GeneRegistration.CAVE_DWELLING.get());
+        drawGeneValues(graphics, Component.translatable("gui.complicated_bees.weatherproof_label"), bee, GeneRegistration.WEATHERPROOF.get());
+    }
+
+    private void drawText(GuiGraphics graphics, Component component, int x, int color) {
+        graphics.drawString(Minecraft.getInstance().font, component, x + getX(), nextLine + getY(), color, false);
+        nextLine += LINE_HEIGHT;
     }
 
     private void drawText(GuiGraphics graphics, Component component, int x, int y, int color) {
         graphics.drawString(Minecraft.getInstance().font, component, x + getX(), y + getY(), color, false);
+    }
+
+    private void lineBreak() {
+        nextLine += LINE_HEIGHT / 2;
     }
 
     private void drawWrappedText(GuiGraphics graphics, int x, int y, int color,  Component... components) {
@@ -106,10 +123,19 @@ public class AnalyzerScrollWidget extends AbstractScrollWidget {
         }
     }
 
+    private void drawGeneValues(GuiGraphics graphics, Component label, ItemStack bee, IGene<?> gene) {
+        IGene<?> active = GeneticHelper.getGene(bee, GeneRegistration.GENE_REGISTRY.getKey(gene), true);
+        IGene<?> inactive = GeneticHelper.getGene(bee, GeneRegistration.GENE_REGISTRY.getKey(gene), false);
+        drawText(graphics, label, 2, nextLine,0xFFFFFF);
+        drawText(graphics, active.getTranslationKey(), ACTIVE_COL, nextLine, active.isDominant() ? 0xE63225 : 0x257FE6);
+        drawText(graphics, inactive.getTranslationKey(), INACTIVE_COL, nextLine, inactive.isDominant() ? 0xE63225 : 0x257FE6);
+        nextLine += LINE_HEIGHT;
+    }
+
     private void drawGeneValues(GuiGraphics graphics, Component label, ItemStack bee, IGene<?> gene, int y) {
         IGene<?> active = GeneticHelper.getGene(bee, GeneRegistration.GENE_REGISTRY.getKey(gene), true);
         IGene<?> inactive = GeneticHelper.getGene(bee, GeneRegistration.GENE_REGISTRY.getKey(gene), false);
-        drawText(graphics, label, 2, y, 0xFFFFFF);
+        drawText(graphics, label, 2, y,0xFFFFFF);
         drawText(graphics, active.getTranslationKey(), ACTIVE_COL, y, active.isDominant() ? 0xE63225 : 0x257FE6);
         drawText(graphics, inactive.getTranslationKey(), INACTIVE_COL, y, inactive.isDominant() ? 0xE63225 : 0x257FE6);
     }
@@ -148,10 +174,12 @@ public class AnalyzerScrollWidget extends AbstractScrollWidget {
         }
     }
 
-    private void drawTolerantGeneValues(GuiGraphics graphics, Component label, ItemStack bee, GeneTolerant<?> gene, int y) {
-        drawGeneValues(graphics, label, bee, gene, y);
-        drawText(graphics, Component.translatable("gui.complicated_bees.tolerance_label"), 9 + TOLERANCE_INDENT, y + LINE_HEIGHT, 0xFFFFFF);
-        drawToleranceIcons(graphics, bee, gene, ACTIVE_COL, INACTIVE_COL, y + LINE_HEIGHT);
-        drawGeneTolerance(graphics, bee, gene, ACTIVE_COL + 10, INACTIVE_COL + 10, y + LINE_HEIGHT);
+    private void drawTolerantGeneValues(GuiGraphics graphics, Component label, ItemStack bee, GeneTolerant<?> gene) {
+        drawGeneValues(graphics, label, bee, gene, nextLine);
+        nextLine += LINE_HEIGHT;
+        drawText(graphics, Component.translatable("gui.complicated_bees.tolerance_label"), 9 + TOLERANCE_INDENT, nextLine, 0xFFFFFF);
+        drawToleranceIcons(graphics, bee, gene, ACTIVE_COL, INACTIVE_COL, nextLine);
+        drawGeneTolerance(graphics, bee, gene, ACTIVE_COL + 10, INACTIVE_COL + 10, nextLine);
+        nextLine += LINE_HEIGHT;
     }
 }
