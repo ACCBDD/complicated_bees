30a629d78dc1cf78882d9408a01da7b78f747d92
ACCBDD <9992251+ACCBDD@users.noreply.github.com>
2024-10-07 01:37:05 -0700

    close #2

--- src/main/java/com/accbdd/complicated_bees/registry/MenuRegistration.java
+++ src/main/java/com/accbdd/complicated_bees/registry/MenuRegistration.java
@@ -23,5 +23,5 @@ public class MenuRegistration {
     public static final Supplier<MenuType<GeneratorMenu>> GENERATOR_MENU = MENU_TYPES.register("generator",
             () -> IForgeMenuType.create(((windowId, inv, data) -> new GeneratorMenu(windowId, inv.player, data.readBlockPos()))));
     public static final Supplier<MenuType<AnalyzerMenu>> ANALYZER_MENU = MENU_TYPES.register("analyzer",
-            () -> IForgeMenuType.create(AnalyzerMenu::fromNetwork));
+            () -> IForgeMenuType.create((windowId, playerInv, data) -> AnalyzerMenu.fromNetwork(windowId, playerInv)));
 }
